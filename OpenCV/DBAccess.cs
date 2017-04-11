aced05172d6839a54ff2cf8732a651815914a873
coolMukul <mukulvarshney@outlook.com>
2017-04-11 17:54:27 +0530

    v1.0

---
+++ OpenCV/DBAccess.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCV
+{
+    public class DBAccess : IDBAccess
+    {
+        public string SaveFace(string username, byte[] faceBlob)
+        {
+            //var connection = ConfigurationManager.ConnectionStrings["Test"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=[server_name];Database=[database_name];Trusted_Connection=true";
+                // using the code here...
+            }
+        }
+
+        public List<Face> CallFaces(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUsernameValid(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaveAdmin(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetUserId(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GenerateUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUsername(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetAllUsernames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenCV/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace OpenCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.txtNeighbors = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScaleFactor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnDetectFaces = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.lblCount);
+            this.panelMain.Controls.Add(this.label3);
+            this.panelMain.Controls.Add(this.txtNeighbors);
+            this.panelMain.Controls.Add(this.label2);
+            this.panelMain.Controls.Add(this.txtScaleFactor);
+            this.panelMain.Controls.Add(this.label1);
+            this.panelMain.Controls.Add(this.btnBrowse);
+            this.panelMain.Controls.Add(this.btnDetectFaces);
+            this.panelMain.Controls.Add(this.pictureBox1);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(771, 504);
+            this.panelMain.TabIndex = 3;
+            // 
+            // txtNeighbors
+            // 
+            this.txtNeighbors.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNeighbors.Location = new System.Drawing.Point(736, 135);
+            this.txtNeighbors.Name = "txtNeighbors";
+            this.txtNeighbors.Size = new System.Drawing.Size(25, 20);
+            this.txtNeighbors.TabIndex = 8;
+            this.txtNeighbors.Text = "3";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(667, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Neighbours";
+            // 
+            // txtScaleFactor
+            // 
+            this.txtScaleFactor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtScaleFactor.Location = new System.Drawing.Point(736, 102);
+            this.txtScaleFactor.Name = "txtScaleFactor";
+            this.txtScaleFactor.Size = new System.Drawing.Size(25, 20);
+            this.txtScaleFactor.TabIndex = 6;
+            this.txtScaleFactor.Text = "1.1";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(667, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Scale Factor";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(670, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(84, 27);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnDetectFaces
+            // 
+            this.btnDetectFaces.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDetectFaces.Location = new System.Drawing.Point(670, 45);
+            this.btnDetectFaces.Name = "btnDetectFaces";
+            this.btnDetectFaces.Size = new System.Drawing.Size(84, 28);
+            this.btnDetectFaces.TabIndex = 3;
+            this.btnDetectFaces.Text = "Detect Faces";
+            this.btnDetectFaces.UseVisualStyleBackColor = true;
+            this.btnDetectFaces.Click += new System.EventHandler(this.btnDetectFaces_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 499);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(667, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Face count:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(733, 206);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(13, 13);
+            this.lblCount.TabIndex = 10;
+            this.lblCount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 504);
+            this.Controls.Add(this.panelMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnDetectFaces;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtScaleFactor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNeighbors;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
